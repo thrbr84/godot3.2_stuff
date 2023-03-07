19d958409e9831adf7ca70f8d995af071456be35
Thiago Bruno <message@thiagobruno.eti.br>
2023-03-07 16:57:01 -0700

    Ajuste no setPrompt()
    Muito obrigado pela alteração!!!
    Fiz apenas um pequeno ajuste ".setPrompt()", retirei o texto e coloquei a variável "title", assim, é possível configurar o título diretamente enviando pela Godot conforme exemplo abaixo.
    
    Valew!!!!
    
    ```
    func _ready():
            qrcode = Engine.get_singleton("GodotQRCode")
            if qrcode:
                    qrcode.setCallbackId(get_instance_id())
    
            qrcode.setTitle("Place the QRCode inside the rectangle to scan it")
            qrcode.setCodeSize(200)
            qrcode.setImageRotated(90)
            qrcode.scanCode()
    ```

--- modules/godotQRCode/src/GodotQRCode.java
+++ modules/godotQRCode/src/GodotQRCode.java
@@ -42,9 +42,8 @@ public class GodotQRCode extends Godot.SingletonBase {
 		IntentIntegrator integrator = new IntentIntegrator(activity);
 		integrator.setRequestCode(REQUEST_IMAGE_CAPTURE);
 		integrator.setOrientationLocked(false);
-		// title
 		integrator.initiateScan();
-		integrator.setPrompt("Scan QR Code");
+		integrator.setPrompt(title);
 	}
 
 	static Bitmap encodeAsBitmap(String contents,
